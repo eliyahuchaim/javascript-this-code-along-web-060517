9c1b79775f63a8cb029c3891da9b6828b0e38fae
github username <github email address>
2017-07-19 15:48:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+
+function Dog(name, breed){
+  this.name = name;
+  this.breed = breed
+};
+
+let weirdDog = new Dog("will", "pitbull")
+//weirdDog.name === "will"
+weirdDog.changeName = function(name) {this.name = name};
+weirdDog.changeName("trey")
+if (weirdDog.name !== "will") {
+  console.log("WUT THU FUK")
+};
+//logs "WUT THU FUK" -- BECUASE THIS IS A WEIRD ASS DOG!
+
+
+function Horse(){
+  arguments
+};
+
+function Horse(){
+	arr = []
+	arr.push(arguments[0]);
+    arr.forEach(function(elem){
+	console.log(elem)
+    //this.elem.keys[0] = elem.values[0]
+    })
+}
